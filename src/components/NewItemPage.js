ca96bba796bf0e80133969da88e373e902b37b50
Robert Lewis <rablewis@algo-pop.com>
2022-02-09 11:57:05 +0000

    Navigate to list after create item

--- src/components/NewItemPage.js
+++ src/components/NewItemPage.js
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import NewItem from './NewItem'
 import {createWidget} from '../api/widgets';
 
 function NewItemPage() {
+    const navigate = useNavigate();
+
+    async function handleCreateItem(widget) {
+        await createWidget(widget);
+        navigate('/widgets', { replace: true });  // 'replace: true' prevents the current route from being included in the browser history
+    }
+
     return (
-        <NewItem createItem={createWidget} />
+        <NewItem createItem={handleCreateItem} />
     );
 }
 
